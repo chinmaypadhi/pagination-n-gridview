3de54c57bbff0fbde9e696461c5ae2b6441fe98a
chinmay padhi <pahichinmay1998@gmail.com>
2021-01-11 19:02:05 +0530

    Add project files.

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace pagination_n_gridview
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridView1.DataSource = SqlDataSource1;
+            GridView1.DataBind();
+        }
+
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            GridView1.DataSource = SqlDataSource1;
+            GridView1.DataBind();
+        }
+    }
+}
